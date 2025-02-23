d212189c46b4dc6add7bcee7d6e362e4c23966cd
yoonkeen <agy91521947@gmail.com>
2025-02-23 22:22:59 +0900

    0.1.2.2-주석 수정

--- src/chap_01/_02_DataTypes.java
+++ src/chap_01/_02_DataTypes.java
@@ -5,9 +5,9 @@ public class _02_DataTypes {
         System.out.println("DataTypes\n안녕하세요"); // sout 이라고 쳐도 이게 만들어짐.
         System.out.println(12);
 //        System.out.println(14, -23, "\n"); // 콤마로 여러개를 묶는것은 안됨.
-        System.out.println(true); // boolian
-        System.out.println(false); //boolian
-        System.out.println(3.14); // float
+        System.out.println(true); // boolean
+        System.out.println(false); //boolean
+        System.out.println(3.14); // double
         System.out.println(-23); // integer
         System.out.println(123+234); // 연산은 가능
     }
---
+++ src/chap_01/_03_Variables.java
@@ -0,0 +1,4 @@
+package chap_01;
+
+public class _03_variables {
+}
