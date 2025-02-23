71fa29614cf971994bd903659a3189220b0535f7
yoonkeen <agy91521947@gmail.com>
2025-02-23 22:10:19 +0900

    0.1.2-DataTypes

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args){ // psvm 또는 main 이라고 치면 자동으로 함수가 만들어짐
+        System.out.println("DataTypes\n안녕하세요"); // sout 이라고 쳐도 이게 만들어짐.
+        System.out.println(12);
+//        System.out.println(14, -23, "\n"); // 콤마로 여러개를 묶는것은 안됨.
+        System.out.println(true); // boolian
+        System.out.println(false); //boolian
+        System.out.println(3.14); // float
+        System.out.println(-23); // integer
+        System.out.println(123+234); // 연산은 가능
+    }
+}
