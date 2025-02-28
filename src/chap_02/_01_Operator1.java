49defb994046032d40ae069a304adaa934645b95
yoonkeen <agy91521947@gmail.com>
2025-02-28 22:04:53 +0900

    0.2.1.1 - Operator1

--- src/chap_02/_01_Operator1.java
+++ src/chap_02/_01_Operator1.java
@@ -35,5 +35,46 @@ public class _01_Operator1 {
 
         c = a%b;
         System.out.println(c);
+
+
+        // 증감 연산 ++, --
+//        ++val; // 증감 뒤 문장 수행
+//        val++ 문장 수행 뒤 증감
+
+        int val;
+        val = 10;
+        System.out.println(val);
+        System.out.println(val++); // 위와 똑같이 10이 출력됨
+        System.out.println(val); // 위 문장이 실행 되고 올라갔기에 11이됨.
+
+        val = 10;
+        System.out.println(val); // 10으로 초기화 했으니 10
+        System.out.println(++val); // 증감이 앞에 있으니 증감을 우선하고 출력
+        System.out.println(val); // 증감된 값이 그대로 출력
+
+        val = 10;
+        System.out.println(val);
+        System.out.println(val--); // 문장 수행이 먼저 되기에 -> 10
+        System.out.println(val); // 위 문장이 실행 되고 올라갔기에 -> 9
+
+        val = 10;
+        System.out.println(val); // 10으로 초기화 했으니 10
+        System.out.println(--val); // 증감이 앞에 있으니 증감을 우선하고 출력 -> 9
+        System.out.println(val); // 증감된 값이 그대로 출력 -> 9
+
+        int waiting = 0;
+        System.out.println("\n대기 인원 : " + waiting++ ); // 대기인원: 0
+        System.out.println("대기 인원 : " + waiting++ ); // 대기인원: 1
+        System.out.println("대기 인원 : " + waiting++ ); // 대기인원: 2
+        System.out.println("대기 인원 : " + waiting++ ); // 대기인원: 3
+        System.out.println("총 대기 인원 : " + waiting);
+
+        waiting = 0;
+        System.out.println("\n대기 인원 : " + ++waiting ); // 대기인원: 1
+        System.out.println("대기 인원 : " + ++waiting ); // 대기인원: 2
+        System.out.println("대기 인원 : " + ++waiting ); // 대기인원: 3
+        System.out.println("대기 인원 : " + ++waiting ); // 대기인원: 4
+        System.out.println("총 대기 인원 : " + waiting);
+
     }
 }
