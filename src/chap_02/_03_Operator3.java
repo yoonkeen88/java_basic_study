c54fdbcc962afbf3a5dfeed4957126c050998a64
yoonkeen <agy91521947@gmail.com>
2025-02-28 22:17:24 +0900

    0.2.3.0 - Operator3

---
+++ src/chap_02/_03_Operator3.java
@@ -0,0 +1,22 @@
+package chap_02;
+
+public class _03_Operator3 {
+    public static void main(String[] args) {
+        System.out.println(5>3); // -> boolean
+        System.out.println(5>=3);
+        System.out.println(5>=5);
+        System.out.println(5>=6);
+
+        System.out.println();
+        System.out.println(5<=5);
+        System.out.println(5<=6);
+
+        System.out.println();
+        System.out.println(5==5);
+        System.out.println(5==3);
+
+        System.out.println();
+        System.out.println(5!=5);
+        System.out.println(5!=3);
+    }
+}
