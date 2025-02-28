4b147980c6beaf6a064aef78812701d45188970a
yoonkeen <agy91521947@gmail.com>
2025-02-28 14:40:18 +0900

    0.1.6.0

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,12 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE= "+82";
+//        KR_COUNTRY_CODE = "8282";828
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592739;
+        System.out.println(PI);
+    }
+}
