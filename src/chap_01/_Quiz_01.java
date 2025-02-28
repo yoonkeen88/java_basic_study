ff4b7400dbf74f5633a0812e1a674fc7a6cf9200
yoonkeen <agy91521947@gmail.com>
2025-02-28 21:23:44 +0900

    0.1.8.0 - Quiz

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,12 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNumber = "상암 028";
+        int time = 4;
+        float distance = 23.4f;
+        System.out.println("버스 정보: "+busNumber +"\n남은 시간: 약 "+ time +"분\n남은 거리: 약 "+ distance+"km");
+
+
+    }
+}
