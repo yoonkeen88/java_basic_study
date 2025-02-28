f50be3eaa3e1de1ace881b81f19fb3f865f427bd
yoonkeen <agy91521947@gmail.com>
2025-02-28 14:44:00 +0900

    0.1.7.0

---
+++ src/chap_01/_07_TypeCasting.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _07_TypeCasting {
+    public static void main(String[] args) {
+        // 형변환 type casting
+        // 정수 -> 실수
+        // 실수 -> 정수
+
+//        int score = 93+89.0; // 이경우는 에러가 발생함 결과가 정수가 아님
+        int score = 93;
+        System.out.println(score);
+        System.out.println((float) score);
+        System.out.println((float) score);
+    }
+}
