d35a8a1db7df4461ab976846d6936b28ccd9d571
yoonkeen <agy91521947@gmail.com>
2025-03-03 23:45:01 +0900

    0.4.2.0

--- src/chap_04/_02_Else.java
+++ src/chap_04/_02_Else.java
@@ -2,6 +2,23 @@ package chap_04;
 
 public class _02_Else {
     public static void main(String[] args) {
+        int hour = 10;
+        if(hour<14){
+            System.out.println("Ice Americano +1 ");
 
+        }else{
+            System.out.println("Decafein Americano +1");
+        }
+        System.out.println("Complete order! #1");
+
+        // 오후 2시 이후거나 모닝커피를 마신 경우
+        hour = 15;
+        boolean morningCoffee = true;
+        if(hour>14 || morningCoffee ==true){
+            System.out.println("Decafein Americano +1");
+        }else{
+            System.out.println("Ice Americano +1");
+        }
+        System.out.println("Complete order! #2");
     }
 }
