f9d8bdc8bc0a28547bb38ccf62cb0bd90f3dbf31
yoonkeen <agy91521947@gmail.com>
2025-03-04 00:31:14 +0900

    0.4.7.0 - DoWhile

--- src/chap_04/_Quiz_04.java
+++ src/chap_04/_Quiz_04.java
@@ -1,4 +1,19 @@
 package chap_04;
 
 public class _Quiz_04 {
+    public static void main(String[] args) {
+        boolean is_changelleger =true;
+        boolean is_small = false;
+        int during = 10;
+        int pay = during * 4000;
+
+        if(pay>30_000){
+            pay = 30000;
+        }
+
+        if(is_changelleger||is_small){
+            pay /=2;
+        }
+        System.out.println("최종 주차 요금 : " + pay + "원 입니다.");
+    }
 }
